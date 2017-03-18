1b32078010378008915055ccd369982b6404eb48
sonu2017 <sonupandey55@yahoo.com>
2017-03-18 18:31:21 +0530

    Adding Pagvier

--- app/src/main/java/com/anghatechsolution/eww/epoojastore/MainActivity.java
+++ app/src/main/java/com/anghatechsolution/eww/epoojastore/MainActivity.java
@@ -1,8 +1,10 @@
 package com.anghatechsolution.eww.epoojastore;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +14,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener ,ViewPager.OnPageChangeListener
+{
+    public ViewPager mPager;
+    private LinearLayout mDotsLayout;
+    private int mDotsCount;
+    static TextView mDotsText[];
+    private int mPosition = 0;
+    private ImageAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +37,32 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        mPager = (ViewPager) findViewById(R.id.image_pagerview);
+        mDotsLayout = (LinearLayout) findViewById(R.id.image_count);
+        mPager.addOnPageChangeListener(this);
+        mAdapter = new ImageAdapter(this);
+        int[] mResources = {R.drawable.android, R.drawable.cover, R.drawable.download, R.drawable.gridview};
+
+        mDotsCount = mResources.length;
+        mDotsText = new TextView[mDotsCount];
+        for (int i = 0; i < mDotsCount; i++) {
+            mDotsText[i] = new TextView(getApplicationContext());
+            mDotsText[i].setText(".");
+            mDotsText[i].setTextSize(45);
+            mDotsText[i].setTypeface(null, Typeface.BOLD);
+            mDotsText[i].setTextColor(android.graphics.Color.GRAY);
+            mDotsLayout.addView(mDotsText[i]);
+        }
+        mAdapter.setList(mResources);
+        mPager.setAdapter(mAdapter);
+        // method to set the selected dot color
+        setSelectedDotColor(mPosition);
+        // Setting the animation for the banner images
+        Timer timer = new Timer();
+        timer.schedule(new UpdateTimeTask(), 5000, 5000);
+
+
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -73,6 +105,17 @@ public class MainActivity extends AppCompatActivity
 
         return super.onOptionsItemSelected(item);
     }
+    private void setSelectedDotColor(int position) {
+        // setting all dot colors to default color
+        for (int i = 0; i < mDotsCount; i++) {
+            mDotsText[i].setTextColor(getApplicationContext().getResources().getColor(
+                    R.color.green));
+        }
+
+        mDotsText[position].setTextColor(getApplicationContext().getResources().getColor(
+                R.color.grey));
+    }
+
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
@@ -97,5 +140,68 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    /**
+     * This method will be invoked when the current page is scrolled, either as part
+     * of a programmatically initiated smooth scroll or a user initiated touch scroll.
+     *
+     * @param position             Position index of the first page currently being displayed.
+     *                             Page position+1 will be visible if positionOffset is nonzero.
+     * @param positionOffset       Value from [0, 1) indicating the offset from the page at position.
+     * @param positionOffsetPixels Value in pixels indicating the offset from position.
+     */
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    /**
+     * This method will be invoked when a new page becomes selected. Animation is not
+     * necessarily complete.
+     *
+     * @param position Position index of the new selected page.
+     */
+    @Override
+    public void onPageSelected(int position)
+    {
+        mPosition = position;
+
+        setSelectedDotColor(position);
+
+    }
+
+    /**
+     * Called when the scroll state changes. Useful for discovering when the user
+     * begins dragging, when the pager is automatically settling to the current page,
+     * or when it is fully stopped/idle.
+     *
+     * @param state The new scroll state.
+     * @see ViewPager#SCROLL_STATE_IDLE
+     * @see ViewPager#SCROLL_STATE_DRAGGING
+     * @see ViewPager#SCROLL_STATE_SETTLING
+     */
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+    class UpdateTimeTask extends TimerTask {
+        public void run() {
+            mPager.post(new Runnable() {
+                public void run() {
+
+                    if (mPager.getCurrentItem() < mAdapter
+                            .getCount() - 1) {
+                        mPager.setCurrentItem(
+                                mPager.getCurrentItem() + 1, true);
+
+
+                    } else {
+                        mPager.setCurrentItem(0, true);
+
+                    }
+                }
+            });
+        }
     }
 }
---
+++ app/src/main/java/com/anghatechsolution/eww/epoojastore/SplashScreen.java
@@ -0,0 +1,42 @@
+package com.anghatechsolution.eww.epoojastore;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by eww on 3/16/2017.
+ */
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
